9f680845868d8f4ba11d28d7f5f9b09acf7ab74d
Veronika <nikapolishuck@yandex.ru>
2022-08-28 21:18:47 +0300

    Secound homework

---
+++ дз2.cs
@@ -0,0 +1,41 @@
+﻿// Задача 10. Напишите программу, которая на вход принимает трехзначное число и на выходе показывает вторую цифру этого числаю
+/*int FindSecoundDigit (int number)
+{
+    int ed =  number % 10;
+    int hund = number / 100;
+
+    int middle;
+    middle = (number - hund * 100 - ed) / 10;
+    return middle;
+}
+
+Console.Write ("Введите число: ");
+int newDigit = Convert.ToInt32(Console.ReadLine());
+int secoundDigit = FindSecoundDigit;
+
+Console.WriteLine($"Вторая цифра из числа {newDigit} это {secoundDigit}");
+*/
+
+//Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что ее нет.
+
+/*int FindThirdDigit (int number)
+{
+    int dec =  number / 10;
+    int hund = number / 100;
+
+    int third;
+    third = number - hund * 100 - dec * 10;
+
+    return middle;
+}
+
+Console.Write ("Введите число: ");
+int newDigit = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = FindThirdDigit;
+
+if ( thirdDigit = 0) Console.WriteLine("Третьей цифры нет");
+else Console.WriteLine (thirdDigit);
+*/
+
+
+
